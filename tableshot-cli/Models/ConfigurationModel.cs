8b4939ee4b82516956a4e4a081588c76365c1b1f
Joël Luijmes <joelluijmes@gmail.com>
2017-06-13 11:51:12 +0200

    parse tables again from config file

--- tableshot-cli/Models/ConfigurationModel.cs
+++ tableshot-cli/Models/ConfigurationModel.cs
@@ -1,4 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace tableshot.Models
 {
@@ -9,11 +13,35 @@ namespace tableshot.Models
 
         [JsonProperty("target")]
         public ServerCredentials TargetCredentials { get; set; }
-
+        
         [JsonProperty("tables")]
-        public CloneTable[] Tables { get; set; }
+        [JsonConverter(typeof(TablesConverter))]
+        public IList<CloneTable> Tables { get; set; }
 
         [JsonProperty("schema")]
         public string Schema { get; set; }
+
+        private class TablesConverter : JsonConverter
+        {
+            public override bool CanWrite => false;
+
+            public override bool CanConvert(Type objectType) => false;
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                var jsonArray = JArray.Load(reader);
+
+                return jsonArray.Select(jsonTable => new CloneTable
+                {
+                    Table = Util.ParseTableName(jsonTable["name"].ToString()),
+                    ReferencedBy = Util.ParseReferencedByOptions(jsonTable["referenced"].ToString())
+                }).ToList();
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
