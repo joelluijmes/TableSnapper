c535d6494ad2226d9edce3d827435efb9be2d5ed
Joël Luijmes <joelluijmes@gmail.com>
2017-07-07 15:33:14 +0200

    CloneCommand inherits DatabaseCommand

--- tableshot-cli/Commands/CloneCommand.cs
+++ tableshot-cli/Commands/CloneCommand.cs
@@ -11,32 +11,30 @@ using tableshot.Models;
 
 namespace tableshot.Commands
 {
-    internal sealed class CloneCommand : ICommand
+    internal sealed class CloneCommand : DatabaseCommand
     {
         private CommandOption _createMissing;
         private CommandOption _checkReferenced;
 
-        public string Name => "clone";
-        public string Description => "clones tables and data between schemas and databases";
+        public override string Name => "clone";
+        public override string Description => "clones tables and data between schemas and databases";
 
-        public void Configure(CommandLineApplication application)
+        public override void Configure(CommandLineApplication application)
         {
             _createMissing = application.Option("--create-missing", "creates missing schemas (default false)", CommandOptionType.NoValue);
             _checkReferenced = application.Option("--check-referenced", "check referenced tables before droping them (default false)", CommandOptionType.NoValue);
         }
 
-        public async Task Execute()
+        protected override async Task Execute(DatabaseManager databaseManager)
         { 
             // read the config
-            var source = Program.Configuration.SourceCredentials.ToConnectionStringBuilder();
             var target = Program.Configuration.TargetCredentials.ToConnectionStringBuilder();
             var tables = Program.Configuration.TableConfigurations;
 
             // do the cloning
-            using (var sourceConnection = await DatabaseConnection.CreateConnectionAsync(source))
             using (var targetConnection = await DatabaseConnection.CreateConnectionAsync(target))
             {
-                var cloner = new DatabaseCloner(sourceConnection, targetConnection);
+                var cloner = new DatabaseCloner(databaseManager.Connection, targetConnection);
                 var options = new DatabaseCloner.DatabaseCloneOptions(tables)
                 {
                     CreateMissingSchemas = _createMissing.HasValue(),
